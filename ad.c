9e4807412a67234cbff2d65c06308907a402a531
Paul J. Lucas <paul@lucasmail.org>
2006-07-07 22:48:04 +0000

    1. Changed tabs to spaces. 2. Fixed little endian bug.

--- ad.c
+++ ad.c
@@ -1,216 +1,242 @@
 #define VERSION "1.1"
 /*
-**	ad -- ASCII dump
-**	ad.c: implementation
+**      ad -- ASCII dump
+**      ad.c: implementation
 **
-**	Copyright (C) 1996  Paul J. Lucas
+**      Copyright (C) 1996  Paul J. Lucas
 **
-**	This program is free software; you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation; either version 2 of the Licence, or
-**	(at your option) any later version.
+**      This program is free software; you can redistribute it and/or modify
+**      it under the terms of the GNU General Public License as published by
+**      the Free Software Foundation; either version 2 of the Licence, or
+**      (at your option) any later version.
 ** 
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+**      This program is distributed in the hope that it will be useful,
+**      but WITHOUT ANY WARRANTY; without even the implied warranty of
+**      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**      GNU General Public License for more details.
 ** 
-**	You should have received a copy of the GNU General Public License
-**	along with this program; if not, write to the Free Software
-**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**      You should have received a copy of the GNU General Public License
+**      along with this program; if not, write to the Free Software
+**      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 /*
-**	Options:
+**      Options:
 **
-**	  -d	Output offset in decimal.
-**	  -o	Output offset in octal.
-**	  -h	Output offset in hexadecimal (default).
-**	  -v	Print version to stderr and exit.
+**          -d  Output offset in decimal.
+**          -o  Output offset in octal.
+**          -h  Output offset in hexadecimal (default).
+**          -v  Print version to stderr and exit.
 */
 
-#include <ctype.h>				/* for isprint() */
-#include <fcntl.h>				/* for O_RDONLY */
+#include <ctype.h>                      /* for isprint() */
+#include <fcntl.h>                      /* for O_RDONLY */
 #include <stdio.h>
-#include <stdlib.h>				/* for exit() */
-#include <string.h>				/* for str...() */
+#include <stdlib.h>                     /* for exit() */
+#include <string.h>                     /* for str...() */
 #include <sys/types.h>
-#include <unistd.h>				/* for lseek(), read(), ... */
+#include <unistd.h>                     /* for lseek(), read(), ... */
 
-#define	BUF_SIZE	16			/* bytes displayed on a line */
+#define BUF_SIZE    16                  /* bytes displayed on a line */
+
+#if !( defined(__STDC__) || defined(__cplusplus) )
+#   define KNR_C
+#endif
+
+#ifdef KNR_C
+#   define const                       /* throw away const */
+#endif
+
+typedef unsigned short ad_uint16;
 
 /*************************/
-	int
-#if defined(__STDC__) || defined(__cplusplus)
-main( int argc, char *argv[] ) {
+    ad_uint16
+#ifdef KNR_C
+flip_int16( i ) ad_uint16 i; {
 #else
-#define const					/* throw away const */
-main( argc, argv ) int argc; char *argv[]; {
+flip_int16( ad_uint16 i ) {
 #endif
-	char const	*me;		 	/* our name */
-	extern char	*optarg;
-	extern int	optind, opterr;
-	int		opt;			/* command-line option */
+    return (i << 8) & 0xFF00 |
+           (i >> 8) & 0x00FF;
+}
 
-	char const *const format_spec[] = {	/* offset format in printf() */
-		"%08lu: ",			/* 0: decimal */
-		"%08lX: ",			/* 1: hex */
-		"%08lo: ",			/* 2: octal */
-	};
-	char const	options[] = "dhov";	/* dho in same order as above */
-	int		which_format = strchr( options, 'h' ) - options;
-	int		plus = 0;		/* specified '+'? */
+/*************************/
+    int
+#ifdef KNR_C
+main( argc, argv ) int argc; char *argv[]; {
+#else
+main( int argc, char *argv[] ) {
+#endif
+    char const  *me;                    /* our name */
+    extern char *optarg;
+    extern int  optind, opterr;
+    int         opt;                    /* command-line option */
 
-	int		fd = 0;			/* Unix file descriptor */
-	off_t		offset = 0L;		/* offset into file */
-	u_char		buf[ BUF_SIZE ];
-	size_t		bytes_read;
+    char const *const format_spec[] = { /* offset format in printf() */
+        "%016llu: ",                    /* 0: decimal */
+        "%016llX: ",                    /* 1: hex */
+        "%016llo: ",                    /* 2: octal */
+    };
+    char const  options[] = "dhov";     /* dho in same order as above */
+    int         which_format = strchr( options, 'h' ) - options;
+    int         plus = 0;               /* specified '+'? */
 
-	/* strip pathname from argv[0] */
-	if ( me = strrchr( argv[0], '/' ) )
-		++me;
-	else
-		me = argv[0];
+    int         fd = 0;                 /* Unix file descriptor */
+    off_t       offset = 0L;            /* offset into file */
+    u_char      buf[ BUF_SIZE ];
+    size_t      bytes_read;
+    int         little_endian;
+    int         i;
 
-	/* ---------- process command line ---------- */
+    /* strip pathname from argv[0] */
+    if ( me = strrchr( argv[0], '/' ) )
+        ++me;
+    else
+        me = argv[0];
 
-	opterr = 1;
-	while ( ( opt = getopt( argc, argv, options ) ) != EOF )
-		switch ( opt ) {
-			case 'd':
-			case 'o':
-			case 'h':
-				which_format = strchr( options, opt ) - options;
-				break;
-			case 'v': goto version;
-			case '?': goto usage;
-		}
-	argc -= optind, argv += optind - 1;
+    /* ---------- determine endianness ---------- */
 
-	switch ( argc ) {
+    union {
+        ad_uint16 s;
+        u_char    c[ sizeof( short ) ];
+    } u;
+    for ( i = 0; i < sizeof( ad_uint16 ); ++i )
+        u.c[i] = i + 1;
+    little_endian = u.s == 0x0201;
 
-		case 0:
-			/*
-			** No arguments: read from stdin with no offset.
-			*/
-			break;
+    /* ---------- process command line ---------- */
 
-		case 1:
-			if ( plus = *argv[1] == '+' ) {
-				/*
-				** The argument has a leading '+' indicating
-				** that it is an offset rather than a file name
-				** and that we should read from stdin.
-				** However, we can't seek() on stdin, so read
-				** and discard offset bytes.
-				*/
-				size_t size = BUF_SIZE;
-				offset = strtol( argv[1], (char**)0, 0 );
-				for ( ;; ) {
-					if ( size > offset )
-						size = offset;
-					if ( !size )
-						break;
-					bytes_read = read( fd, buf, size );
-					if ( bytes_read == -1 )
-						break;
-					offset -= bytes_read;
-				}
-				break;
-			}
-			/* no break; */
+    opterr = 1;
+    while ( ( opt = getopt( argc, argv, options ) ) != EOF )
+        switch ( opt ) {
+            case 'd':
+            case 'o':
+            case 'h':
+                which_format = strchr( options, opt ) - options;
+                break;
+            case 'v': goto version;
+            case '?': goto usage;
+        }
+    argc -= optind, argv += optind - 1;
 
-		case 2:
-			if ( argc == 2 ) {
-				/*
-				** There really are two arguments (we didn't
-				** fall through from the above case): the 2nd
-				** argument is the offset.
-				*/
-				offset = strtol( argv[2], (char**)0, 0 );
+    switch ( argc ) {
 
-			} else if ( plus ) {
-				/*
-				** There is only one argument (we fell through
-				** from the above case) and a '+' was given,
-				** hence that single argument was an offset and
-				** we should read from stdin.
-				*/
-				break;
-			}
+        case 0:
+            /*
+            ** No arguments: read from stdin with no offset.
+            */
+            break;
 
-			/*
-			** The first (and perhaps only) argument is a file
-			** name: open the file and seek to the proper offset.
-			*/
-			if ( ( fd = open( argv[1], O_RDONLY ) ) == -1 ) {
-				fprintf( stderr, "%s: can not open %s\n",
-					me, argv[1]
-				);
-				exit( 2 );
-			}
-			if ( lseek( fd, offset, 0 ) == -1 ) {
-				fprintf( stderr, "%s: can not seek to %ld\n",
-					me, offset
-				);
-				exit( 3 );
-			}
-			break;
+        case 1:
+            if ( plus = *argv[1] == '+' ) {
+                /*
+                ** The argument has a leading '+' indicating that it is an
+                ** offset rather than a file name and that we should read from
+                ** stdin.  However, we can't seek() on stdin, so read and
+                ** discard offset bytes.
+                */
+                size_t size = BUF_SIZE;
+                offset = strtol( argv[1], (char**)0, 0 );
+                for ( ;; ) {
+                    if ( size > offset )
+                        size = offset;
+                    if ( !size )
+                        break;
+                    bytes_read = read( fd, buf, size );
+                    if ( bytes_read == -1 )
+                        break;
+                    offset -= bytes_read;
+                }
+                break;
+            }
+            /* no break; */
 
-		default:
-			goto usage;
-	}
+        case 2:
+            if ( argc == 2 ) {
+                /*
+                ** There really are two arguments (we didn't fall through from
+                ** the above case): the 2nd argument is the offset.
+                */
+                offset = strtol( argv[2], (char**)0, 0 );
 
-	/* ---------- main processing ---------- */
+            } else if ( plus ) {
+                /*
+                ** There is only one argument (we fell through from the above
+                ** case) and a '+' was given, hence that single argument was an
+                ** offset and we should read from stdin.
+                */
+                break;
+            }
 
-	while ( ( bytes_read = read( fd, buf, BUF_SIZE ) ) > 0 ) {
+            /*
+            ** The first (and perhaps only) argument is a file name: open the
+            ** file and seek to the proper offset.
+            */
+            if ( ( fd = open( argv[1], O_RDONLY ) ) == -1 ) {
+                fprintf( stderr, "%s: can not open %s\n", me, argv[1] );
+                exit( 2 );
+            }
+            if ( lseek( fd, offset, 0 ) == -1 ) {
+                fprintf( stderr, "%s: can not seek to %ld\n", me, offset );
+                exit( 3 );
+            }
+            break;
 
-		register u_short const *pword = (u_short*)buf;
-		register u_char  const *pchar = (u_char*)buf;
-		register int i;
+        default:
+            goto usage;
+    }
 
-		/* print offset */
-		printf( format_spec[ which_format ], offset );
+    /* ---------- main processing ---------- */
 
-		/* print hex part */
-		for ( i = bytes_read / 2; i; --i, ++pword )
-			printf( "%04X ", *pword );
-		if ( bytes_read & 1 ) {
-			/* there's an odd, left-over byte */
-			printf( "%02X ", (u_short)*(u_char*)pword );
-		}
+    while ( ( bytes_read = read( fd, buf, BUF_SIZE ) ) > 0 ) {
 
-		/* print padding if we read less than BUF_SIZE bytes */
-		for ( i = BUF_SIZE - bytes_read; i; --i )
-			printf( "  " );
-		for ( i = (BUF_SIZE - bytes_read) / 2; i; --i )
-			putchar( ' ' );
+        register ad_uint16 const *pword = (ad_uint16*)buf;
+        register u_char    const *pchar = (u_char*)buf;
+        register int i;
 
-		/* separator */
-		putchar( ' ' );
+        /* print offset */
+        printf( format_spec[ which_format ], offset );
 
-		/* print ascii part */
-		for ( i = 0; i < bytes_read; ++i, ++pchar )
-			putchar( (char)( isprint( *pchar ) ? *pchar : '.' ) );
+        /* print hex part */
+        for ( i = bytes_read / 2; i; --i, ++pword )
+            printf( "%04X ", little_endian ? flip_int16( *pword ) : *pword );
+        if ( bytes_read & 1 ) {
+            /* there's an odd, left-over byte */
+            printf( "%02X ", (ad_uint16)*(u_char*)pword );
+        }
 
-		putchar( '\n' );
+        /* print padding if we read less than BUF_SIZE bytes */
+        for ( i = BUF_SIZE - bytes_read; i; --i )
+            printf( "  " );
+        for ( i = (BUF_SIZE - bytes_read) / 2; i; --i )
+            putchar( ' ' );
 
-		if ( bytes_read < BUF_SIZE )
-			break;
+        /* separator */
+        putchar( ' ' );
 
-		offset += BUF_SIZE;
-	}
+        /* print ascii part */
+        for ( i = 0; i < bytes_read; ++i, ++pchar )
+            putchar( (char)( isprint( *pchar ) ? *pchar : '.' ) );
 
-	/* ---------- done ---------- */
+        putchar( '\n' );
 
-	close( fd );
-	exit( 0 );
+        if ( bytes_read < BUF_SIZE )
+            break;
+
+        offset += BUF_SIZE;
+    }
+
+    /* ---------- done ---------- */
+
+    close( fd );
+    exit( 0 );
 
 usage:
-	fprintf( stderr, "usage: %s [-dhov] [ file ] [ [+]offset ]\n", me );
-	exit( 1 );
+    fprintf( stderr, "usage: %s [-dhov] [ file ] [ [+]offset ]\n", me );
+    exit( 1 );
 
 version:
-	fprintf( stderr, "%s: version %s\n", me, VERSION );
-	exit( 0 );
+    fprintf( stderr, "%s: version %s\n", me, VERSION );
+    exit( 0 );
 }
+/* vim:set et sw=4 ts=4: */
