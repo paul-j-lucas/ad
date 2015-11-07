7106f7a36152af32efa2f288074a324898ba3236
Paul J. Lucas <plucas@splunk.com>
2015-11-06 16:05:25 -0800

    Now using SCNu64.

--- src/reverse.c
+++ src/reverse.c
@@ -27,7 +27,7 @@
 // standard
 #include <assert.h>
 #include <ctype.h>
-#include <inttypes.h>                   /* for PRIu64 */
+#include <inttypes.h>                   /* for SCNu64 */
 #include <stdio.h>
 #include <stdlib.h>                     /* for exit() */
 #include <string.h>                     /* for str...() */
@@ -93,7 +93,7 @@ static row_kind_t parse_row( size_t line, char const *buf, size_t buf_len,
   if ( strncmp( buf, elided_separator, ROW_SIZE ) == 0 ) {
     col += OFFSET_WIDTH;
     uint64_t delta;
-    if ( sscanf( buf + OFFSET_WIDTH, ": (%" PRIu64 " | 0x%*X)", &delta ) != 1 )
+    if ( sscanf( buf + OFFSET_WIDTH, ": (%" SCNu64 " | 0x%*X)", &delta ) != 1 )
       INVALID_EXIT(
         "expected '%c' followed by elided counts \"%s\"\n", ':', "(DD | 0xHH)"
       );
