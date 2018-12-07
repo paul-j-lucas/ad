0fed0d0e2e85b40ab48235695d0e225d2581a7fd
Paul J. Lucas <plucas@splunk.com>
2018-12-07 10:54:57 -0800

    Updated copyright year.

--- src/color.h
+++ src/color.h
@@ -2,7 +2,7 @@
 **      ad -- ASCII dump
 **      src/color.h
 **
-**      Copyright (C) 2015-2017  Paul J. Lucas
+**      Copyright (C) 2015-2018  Paul J. Lucas
 **
 **      This program is free software: you can redistribute it and/or modify
 **      it under the terms of the GNU General Public License as published by
