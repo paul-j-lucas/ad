074609445f73f0d69524db0a976708321398f8eb
Paul J. Lucas <paul@lucasmail.org>
2024-03-21 08:50:01 -0700

    Added #include <unistd.h> for STDOUT_FILENO.

--- src/options.c
+++ src/options.c
@@ -38,6 +38,7 @@
 #include <sys/stat.h>                   /* for fstat() */
 #include <sys/types.h>
 #include <sysexits.h>
+#include <unistd.h>                     /* for STDOUT_FILENO */
 
 // Undefine these since they clash with our command-line options.
 #ifdef BIG_ENDIAN
