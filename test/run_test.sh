a1c8461c27f9e13365a6a6609a5a9ec1940a6b8a
Paul J. Lucas <paul@lucasmail.org>
2026-01-04 06:39:20 -0800

    Updated copyright year.

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,7 +3,7 @@
 #       ad -- ASCII dump
 #       test/run_test.sh
 #
-#       Copyright (C) 2015-2025  Paul J. Lucas
+#       Copyright (C) 2015-2026  Paul J. Lucas
 #
 #       This program is free software: you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
